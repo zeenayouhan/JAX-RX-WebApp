ade7572717f66e4521853da4451faf438a36fc58
zeenayouhan <zinayouhan33@gmail.com>
2022-01-10 20:28:43 +0530

    added genericdemo

--- webapp/src/main/java/com/zina/webapp/rest/client/GenericDemo.java
+++ webapp/src/main/java/com/zina/webapp/rest/client/GenericDemo.java
@@ -1,7 +1,10 @@
 package com.zina.webapp.rest.client;
 
+import com.zina.webapp.messenger.model.Message;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
@@ -10,11 +13,11 @@ public class GenericDemo {
     public static void main(String[] args) {
         Client client = ClientBuilder.newClient();
 
-        List response = client.target("http://localhost:8080/webapp_war_exploded/api/")
+        List<Message> messages = client.target("http://localhost:8080/webapp_war_exploded/api/")
                 .path("messages")
-                .queryParam("year",2021)
+                .queryParam("year",2022)
                 .request(MediaType.APPLICATION_JSON)
-                .get(List.class);
-        System.out.println();
+                .get(new GenericType<List< Message >>(){});
+        System.out.println(messages);
     }
 }
