510eccdc95e020ac35b5c8c0fc7134cf6e0bc9da
zeenayouhan <zinayouhan33@gmail.com>
2022-01-10 21:08:23 +0530

    added response filter

---
+++ webapp/src/main/java/com/zina/webapp/PoweredByResponseFilter.java
@@ -0,0 +1,15 @@
+package com.zina.webapp;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import java.io.IOException;
+
+public class PoweredByResponseFilter  implements ContainerResponseFilter {
+    @Override
+    public void filter(ContainerRequestContext requestContext,
+                       ContainerResponseContext responseContext) throws IOException {
+        responseContext.getHeaders().add("X-Powered-By","Zeena");
+
+    }
+}
