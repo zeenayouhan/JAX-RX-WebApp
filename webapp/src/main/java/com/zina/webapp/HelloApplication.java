5ec6aa390c33131599b5c6056ec803147b70e3cd
zeenayouhan <zinayouhan33@gmail.com>
2022-01-10 09:34:58 +0530

    count

--- webapp/src/main/java/com/zina/webapp/HelloApplication.java
+++ webapp/src/main/java/com/zina/webapp/HelloApplication.java
@@ -2,8 +2,15 @@ package com.zina.webapp;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
 @ApplicationPath("/api")
 public class HelloApplication extends Application {
+
+//    public Set<Class<?>> getClasses(){
+//        return new HashSet<Class<?>>();
+//
+//    }
 
 }
--- webapp/src/main/java/com/zina/webapp/TestResource.java
+++ webapp/src/main/java/com/zina/webapp/TestResource.java
@@ -1,14 +1,18 @@
 package com.zina.webapp;
 
+import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-@Path("/hello-world7")
+@Path("/hello-world")
+@Singleton
 public class TestResource {
+    private int count;
     @GET
     @Produces("text/plain")
     public String hello() {
-        return "Hello, World!";
+        count = count+1;
+        return "Hello, World!"+ count;
     }
 }
